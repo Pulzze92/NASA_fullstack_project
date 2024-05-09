d6f19e933b5713e2f59ebac41658d9bcb2290259
Pulzze92 <aleksandr.bakshtaev@gmail.com>
2024-05-10 00:05:11 +0200

    feat: added validation

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Explanation X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
-  customer: ["ZTM", "NASA"],
+  target: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true
 };
@@ -17,6 +19,20 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["ZTM", "NASA"],
+      flightNumber: latestFlightNumber
+    })
+  );
+}
+
 module.exports = {
-  getAllLaunches
+  getAllLaunches,
+  addNewLaunch
 };
