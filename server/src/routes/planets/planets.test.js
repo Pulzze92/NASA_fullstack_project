c9429b3aaf9621461297affd2ae24a15cb92a5ff
Pulzze92 <aleksandr.bakshtaev@gmail.com>
2024-05-10 15:24:36 +0200

    feat: added tests for planets and launches

---
+++ server/src/routes/planets/planets.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /planets", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/planets")
+      .expect("Content-Type", /json/)
+      .expect(200);
+    expect(response.status).toBe(200);
+  });
+});
